ad7680dc9e128454a3e06e345441aee44ad6a12b
Dennis Boon <dennis.boon96@gmail.com>
2019-10-02 16:16:06 +0200

    Implemented color and shape settings; implemented noise filters

--- ProceduralPlanets_OQ/Assets/_Scripts/TerrainFace.cs
+++ ProceduralPlanets_OQ/Assets/_Scripts/TerrainFace.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class TerrainFace
 {
+    ShapeGenerator shapeGenerator;
     Mesh mesh;
     int resolution;
     Vector3 localUp, axisA, axisB;
 
-    public TerrainFace(Mesh mesh, int resolution, Vector3 localUp)
+    public TerrainFace(ShapeGenerator shapeGenerator, Mesh mesh, int resolution, Vector3 localUp)
     {
+        this.shapeGenerator = shapeGenerator;
         this.mesh = mesh;
         this.resolution = resolution;
         this.localUp = localUp;
@@ -32,7 +34,7 @@ public class TerrainFace
                 Vector2 percent = new Vector2(x, y) / (resolution - 1);
                 Vector3 pointOnUnitCube = localUp + (percent.x - .5f) * 2 * axisA + (percent.y - .5f) * 2 * axisB;
                 Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
-                vertices[i] = pointOnUnitSphere;
+                vertices[i] = shapeGenerator.CalculatePointOnPlanet(pointOnUnitSphere);
 
                 if (x != resolution - 1 && y != resolution - 1)
                 {
