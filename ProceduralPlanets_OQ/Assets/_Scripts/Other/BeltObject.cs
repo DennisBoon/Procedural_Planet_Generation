38f074133cf915a55a883fd16055d2eff81b8e66
Dennis Boon <dennis.boon96@gmail.com>
2019-10-15 11:42:35 +0200

    Added 2 scripts to create an asteroid belt and changed planet settings to 2 resolutions

---
+++ ProceduralPlanets_OQ/Assets/_Scripts/Other/BeltObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltObject : MonoBehaviour
+{
+    [SerializeField]
+    private float orbitSpeed;
+    [SerializeField]
+    private GameObject parent;
+    [SerializeField]
+    private bool rotationClockwise;
+    [SerializeField]
+    private float rotationSpeed;
+    [SerializeField]
+    private Vector3 rotationDirection;
+
+    public void SetupBeltObject(float _speed, float _rotationSpeed, GameObject _parent, bool _rotateClockwise)
+    {
+        orbitSpeed = _speed;
+        rotationSpeed = _rotationSpeed;
+        parent = _parent;
+        rotationClockwise = _rotateClockwise;
+        rotationDirection = new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+    }
+
+    private void Update()
+    {
+        if (rotationClockwise)
+        {
+            transform.RotateAround(parent.transform.position, parent.transform.up, orbitSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.RotateAround(parent.transform.position, -parent.transform.up, orbitSpeed * Time.deltaTime);
+        }
+
+        transform.Rotate(rotationDirection, rotationSpeed * Time.deltaTime);
+    }
+}
