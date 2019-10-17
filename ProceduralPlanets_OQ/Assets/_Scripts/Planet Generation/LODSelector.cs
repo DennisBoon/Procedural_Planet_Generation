9b4a5057c589657ff9af6075b1a6e94529e7f143
Dennis Boon <dennis.boon96@gmail.com>
2019-10-17 16:57:21 +0200

    Implement simple system that checks the distance of the player to the planet and swaps the LOD level accordingly

--- ProceduralPlanets_OQ/Assets/_Scripts/Planet Generation/LODSelector.cs
+++ ProceduralPlanets_OQ/Assets/_Scripts/Planet Generation/LODSelector.cs
@@ -5,4 +5,36 @@ using UnityEngine;
 public class LODSelector : MonoBehaviour
 {
     public List<GameObject> LODLevelObjects = new List<GameObject>();
+    public GameObject player;
+    [SerializeField]
+    private GameObject activeLOD;
+    public int[] LODDistances;
+
+    private void Update()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) > LODDistances[0])
+        {
+            SwapLOD(LODLevelObjects[0]);
+        }
+        else if (Vector3.Distance(player.transform.position, transform.position) < LODDistances[0] &&
+                Vector3.Distance(player.transform.position, transform.position) > LODDistances[1])
+        {
+            SwapLOD(LODLevelObjects[1]);
+        }
+        else if (Vector3.Distance(player.transform.position, transform.position) < LODDistances[1] &&
+                Vector3.Distance(player.transform.position, transform.position) > LODDistances[2])
+        {
+            SwapLOD(LODLevelObjects[2]);
+        }
+    }
+
+    void SwapLOD(GameObject obj)
+    {
+        foreach (GameObject i in LODLevelObjects)
+        {
+            i.SetActive(false);
+        }
+        obj.SetActive(true);
+        activeLOD = obj;
+    }
 }
