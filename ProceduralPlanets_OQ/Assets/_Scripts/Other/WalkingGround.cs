14b582c4a9bf1d7bcfcc16fb1b0798055f4c56e7
Dennis Boon <dennis.boon96@gmail.com>
2019-10-29 18:41:58 +0100

    Changed some things to make the player controller work better

---
+++ ProceduralPlanets_OQ/Assets/_Scripts/Other/WalkingGround.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingGround : MonoBehaviour
+{
+    public Transform player;
+
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, transform.position.y, player.position.z);
+    }
+}
